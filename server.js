98e513a2d363959c4cc8eafce8bfd5eae5cd70d5
Kimberlee Johnson <kimee.johnson@gmail.com>
2019-06-03 13:56:53 -0700

    Added server.use

--- server.js
+++ server.js
@@ -2,10 +2,21 @@ const express = require('express');
 
 const server = express();
 
+server.use(express.json()); 
+
 // Calling in my data 
 const Accounts = require('./data/accounts-model.js'); 
 
 // POST (C IN CRUD)
+server.post('/', async (req, res) => {
+    try {
+        const newAccount = await Accounts.add(req.body); 
+        res.status(200).json(newAccount); 
+    } catch (error) {
+        console.log(error); 
+        res.status(500).json({message: "Slow down, partner! Error adding the account."})
+    }
+})
 
 // GET (R IN CRUD)
 server.get('/', async (req, res) => {
@@ -14,7 +25,7 @@ server.get('/', async (req, res) => {
         res.status(200).json(accounts); 
     } catch (error) {
         console.log(error); 
-        res.status(500).json({message: "Whoopsie daisy! Error adding the project."})
+        res.status(500).json({message: "Whoopsie daisy! Error retrieving the accounts."})
     }
 })
 
