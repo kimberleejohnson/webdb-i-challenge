e9a123c5059ff24eb64f674ae1b0abe934aa4090
Kimberlee Johnson <kimee.johnson@gmail.com>
2019-06-03 13:15:08 -0700

    Synced up accounts table

--- server.js
+++ server.js
@@ -2,10 +2,21 @@ const express = require('express');
 
 const server = express();
 
+// Calling in my data 
+const Accounts = require('./data/accounts-model.js'); 
+
 // POST (C IN CRUD)
 
 // GET (R IN CRUD)
-router.get('/')
+server.get('/', async (req, res) => {
+    try {
+        const accounts = await Accounts.find(); 
+        res.status(200).json(accounts); 
+    } catch (error) {
+        console.log(error); 
+        res.status(500).json({message: "Whoopsie daisy! Error adding the project."})
+    }
+})
 
 // PUT (U IN CRUD)
 
